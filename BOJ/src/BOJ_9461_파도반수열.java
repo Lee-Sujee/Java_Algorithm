c2a88e28af6706da1d9a9fda2627f29afa54efe0
Sujee Lee <tnwl5162@naver.com>
2025-10-28 22:22:13 +0900

    1028

---
+++ BOJ/src/BOJ_9461_파도반수열.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+public class BOJ_9461_파도반수열 {
+	public static void main(String[] args) throws IOException  {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int T = Integer.parseInt(br.readLine());
+		List<Long> l = new ArrayList<>();
+		l.add((long) 1);
+		l.add((long)1);
+		l.add((long)1);
+		l.add((long)2);
+		l.add((long)2);
+		int idx = 0;
+		for(int t = 0; t < T; t++) {
+			int x = Integer.parseInt(br.readLine());
+			
+			if(x < l.size()) {
+				System.out.println(l.get(x-1));
+			}
+			else {
+				while(x > l.size()) {
+					l.add(l.get(l.size()-1) + l.get(idx++));
+				}
+				System.out.println(l.get(x-1));
+			}
+		}
+		System.out.println(l);
+	}
+}
